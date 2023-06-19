08b3474f56df50740c53ce386bcf6700f9370eb8
FarhanFDjabari <djabari.dev@gmail.com>
2023-06-19 16:24:00 +0700

    add item list page

--- lib/configs/pages/main_page.dart
+++ lib/configs/pages/main_page.dart
@@ -2,6 +2,8 @@ import 'package:get/route_manager.dart';
 import 'package:trainee/configs/routes/main_route.dart';
 import 'package:trainee/modules/features/counter/binddings/conter_bindding.dart';
 import 'package:trainee/modules/features/counter/views/ui/conter_view.dart';
+import 'package:trainee/modules/features/list/bindings/list_binding.dart';
+import 'package:trainee/modules/features/list/view/list_item_view.dart';
 
 abstract class MainPage {
   static final main = [
@@ -10,6 +12,11 @@ abstract class MainPage {
       name: MainRoute.initial,
       page: () => const ConterView(),
       binding: ConterBindding(),
+    ),
+    GetPage(
+      name: MainRoute.list,
+      page: () => const ListItemView(),
+      binding: ListBinding(),
     ),
   ];
 }
